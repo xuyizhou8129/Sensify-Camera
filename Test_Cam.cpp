b26e4885ba9884f3dd92b639b9d6b94d3c613040
cjhandchy <zhouxuyi@shphshool.com>
2025-02-21 18:49:10 -0600

    Modify Test_Cam

--- Test_Cam.cpp
+++ Test_Cam.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
-using namespace std;
+#include <opencv2/opencv.hpp>
+using namespace cv;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    VideoCapture cap(0); // Open the default camera
+    if (!cap.isOpened()) {
+        return -1;
+    }
+
+    Mat frame;
+    while (true) {
+        cap >> frame; // Capture a new frame
+        if (frame.empty()) {
+            break;
+        }
+        imshow("Camera", frame); // Display the frame
+        if (waitKey(30) >= 0) {
+            break;
+        }
+    }
     return 0;
 }
