3d24736bf77a4c91264333701fad571cf6560246
Shane Deng <shanedengwisc@gmail.com>
2025-02-25 20:57:17 -0600

    Changes

--- sensify_camera.py
+++ sensify_camera.py
@@ -1,4 +1,40 @@
-import sys
-import cv2 as cv
-sys.path.append('/usr/local/lib/python3.9.6/site-packages') 
-img = cv.imread("/Users/zhouyanfeng/Desktop/magi.png")
+# import cv2
+
+# cap = cv2.VideoCapture(0);
+
+# if not cap.isOpened():
+	# print("Error:Could not open webcam.")
+	# exit()
+
+# while(True):
+	# ret,frame = cap.read()
+
+# if ret:
+	# cv2.imwrite('photo.jpg',frame)
+	# cv2.imwrite('photo.jpg',frame)
+	# print("Photo saved")
+# else:
+	# print("Error: Could not read frame from webcam.")
+	
+# cap.release()
+
+# from picamera2 import Picamera2
+# import cv2
+# picam = Picamera2()
+# picam.preview_configuration.controls.FrameRate = 30
+# picam.start()
+
+# while True:
+	# frame = picam.capture_array()
+	# cv2.imshow("steam", frame)
+	
+	
+	
+# cv2.destroyAllWindows()
+# picam.stop()
+
+from picamzero import Camera
+from time import sleep
+cam = Camera()
+cam.start_preview()
+sleep(5)
